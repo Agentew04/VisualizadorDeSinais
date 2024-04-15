e943c781f08bcec5862757cee0cf9771e444c321
Rodrigo Appelt <31523279+Agentew04@users.noreply.github.com>
2024-04-15 20:35:15 -0300

    coisas

---
+++ Codificacoes/ICodificacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizadorDeSinais.Codificacoes {
+    internal interface ICodificacao {
+        public string Codificar(List<char> bitSequence);
+        public string Decodificar(List<char> bitSequence);
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,29 +1,72 @@
-﻿using LiveCharts.Wpf;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace VisualizadorDeSinais {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace VisualizadorDeSinais;
 
-        public Series Series { get; set; }
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        // componentes do front estao disponiveis apos essa call
+        InitializeComponent(); 
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            bitSequenceTextBox.Text = bitSequenceTextBox.Text.Replace(" ", "");
-        }
+        SeriesCollection = new SeriesCollection
+{
+            new LineSeries
+            {
+                Title = "Series 1",
+                Values = new ChartValues<double> { 4, 6, 5, 2 ,7 }
+            },
+            new LineSeries
+            {
+                Title = "Series 2",
+                Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
+            }
+        };
+
+        Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+        YFormatter = value => value.ToString("C");
+
+        //modifying the series collection will animate and update the chart
+        SeriesCollection.Add(new LineSeries {
+            Values = new ChartValues<double> { 5, 3, 2, 4 },
+            LineSmoothness = 0 //straight lines, 1 really smooth lines
+        });
+
+        //modifying any series values will also animate and update the chart
+        SeriesCollection[2].Values.Add(5d);
+
+        DataContext = this;
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var item in bitSequenceTextBox.Text) {
+            if (item == '0' || item == '1') {
+                sb.Append(item);
+            }
+        }
+        List<char> bitSeq = sb.ToString().ToList();
+
+        if (codificacaoComboBox.SelectedItem is not ComboBoxItem selected) {
+            MessageBox.Show(
+                "Selecione um modo de codificação",
+                "Erro!",
+                MessageBoxButton.OK,
+                MessageBoxImage.Exclamation
+            );
+            return;
+        }
+
+    }
+
+    public SeriesCollection SeriesCollection { get; set; }
+    public string[] Labels { get; set; }
+    public Func<double, string> YFormatter { get; set; }
+
 }
